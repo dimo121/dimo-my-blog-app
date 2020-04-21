e7df34ce1e45fc61bdd351a51abcfc09de88e46e
Dimo <dimopapadopoulos82@gmail.com>
2020-04-21 16:55:13 +1000

    Added entry form page tests

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
     <header className="container">
-        <h1 className="nav__title">ReactME blog centre</h1>
+        <h1 className="nav__title">ReactJS blog centre</h1>
         <div className="nav__container">
             <NavLink className="nav__links" to="/" exact={true}>Dashboard</NavLink>
             <NavLink className="nav__links" to="/createblog">Create blog</NavLink>
